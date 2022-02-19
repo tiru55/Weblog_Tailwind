99e861abc478ce869a75269d6e76c3f528d6197c
Ubuntu <ubuntu@ip-172-31-3-93.us-east-2.compute.internal>
2022-02-19 06:58:02 +0000

    Changed Question Cards

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,7 +10,7 @@ class QuestionsController < ApplicationController
 
     def create
         @question = Question.new(question_params)
-        @question.user_id = current_user.user_id
+        @question.user_id = current_user.id
         if @question.save
             flash[:notice] = "Your Question has been Posted.."
             redirect_to @question
