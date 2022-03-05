3b82c6eb9670bd5294fa1f91a7e997ecfa3f5568
Ubuntu <ubuntu@ip-172-31-3-93.us-east-2.compute.internal>
2022-03-05 13:22:53 +0000

    Prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.secret_key_base =  "562a2ebe614c30c483044988f4608355ad2b2b01174b1aac7db9250f2c5966e08cdb5df6f5221b5e9e5284e5d915f339ebfd878e0a1f0608fc685909946d1e6a"
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
