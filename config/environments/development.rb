513bb398f40fabea22d084ead772f011840d7775
Ubuntu <ubuntu@ip-172-31-3-93.us-east-2.compute.internal>
2022-03-05 12:47:11 +0000

    Changes to Application.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,6 @@ config.action_mailer.smtp_settings = {
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.assets.initialize_on_precompile = false
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
